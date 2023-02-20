e8e37bda6f85d7b84f795c984ffcd831f820c5b7
HannesH <33062605+HannesGitH@users.noreply.github.com>
2023-02-20 01:19:11 +0100

    bg

--- lib/pages/buzzmode.dart
+++ lib/pages/buzzmode.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../generated/l10n.dart';
 import '../models/vibration/vibration.dart';
+import '../widgets/pattern/pattern.dart';
 
 class BuzzModePage extends ConsumerWidget {
   const BuzzModePage({Key? key}) : super(key: key);
@@ -23,22 +27,66 @@ class BuzzModePage extends ConsumerWidget {
         onTapUp: (details) {
           ref.read(activeVibrationPatternProvider.notifier).stopVib();
         },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                  pattern.isCurrentlyVibrating ? Icons.stop : Icons.play_arrow),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  pattern.isCurrentlyVibrating
-                      ? S.of(context).buzzmodeStopInstruction
-                      : S.of(context).buzzmodeInstruction,
+        onTapCancel: () {
+          ref.read(activeVibrationPatternProvider.notifier).stopVib();
+        },
+        splashColor: Theme.of(context).colorScheme.primary,
+        child: Stack(
+          children: [
+            IgnorePointer(
+              child: MyLineChart(
+                data: pattern.points,
+                min: Point(
+                    pattern.elements.isEmpty
+                        ? 0
+                        : pattern.elements.first.durationMS,
+                    0),
+                max: Point(
+                  pattern.totalDurationMS,
+                  MAX_VIBRATION_AMPLITUDE,
                 ),
-              )
-            ],
-          ),
+                animationDuration: pattern.doNotAnimate ? 5 : 100,
+                showDot: pattern.isCurrentlyVibrating,
+              ),
+            ),
+            BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container()),
+            Container(
+                color: pattern.isCurrentlyVibrating
+                    ? Theme.of(context)
+                        .colorScheme
+                        .primary
+                        .withAlpha((pattern.points.first.y ~/ 2))
+                    : Theme.of(context)
+                        .colorScheme
+                        .background
+                        .withOpacity(0.5)),
+            Center(
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(pattern.isCurrentlyVibrating
+                          ? Icons.stop
+                          : Icons.play_arrow),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          pattern.isCurrentlyVibrating
+                              ? S.of(context).buzzmodeStopInstruction
+                              : S.of(context).buzzmodeInstruction,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
